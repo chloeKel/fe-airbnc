9f843e9b2987a4250461dc7c5a0f3ca65efa5cca
legshog <chloekelleher@hotmail.co.uk>
2025-03-05 23:07:03 +0000

    fixed favourites button, works for backend but the onclick asset change does not work yet...

--- src/Components/ExplorePage/PropertyCards.jsx
+++ src/Components/ExplorePage/PropertyCards.jsx
@@ -6,14 +6,14 @@ export default function PropertyCards({ properties }) {
   return (
     <PropertyList>
       {properties.map((property, index) => {
-        const { property_id, property_name, location, price_per_night, images, favourited } = property;
+        const { property_id, property_name, location, price_per_night, images, favourited, favourite_id } = property;
         return (
           <PropertyCard key={`${property_id}-${index}`}>
             <PropertyImage src={images[0]} alt={property_name} />
             <Link to={`property/${property_id}`}>{property_name}</Link>
             <p>{location}</p>
             <p>£{price_per_night} per night</p>
-            <ToggleFavourite favourited={favourited} />
+            <ToggleFavourite favourited={favourited} propertyId={property_id} favouriteId={favourite_id} />
           </PropertyCard>
         );
       })}
--- src/Components/Favourites/ToggleFavourite.jsx
+++ src/Components/Favourites/ToggleFavourite.jsx
@@ -1,12 +1,21 @@
 import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../Contexts/Contexts";
 import { FavouriteButton } from "../../Styling/StyledPropertyCard";
+import { postFavourite, deleteFavourite } from "../../Utils/api";
 
-export default function ToggleFavourite({ favourited }) {
+export default function ToggleFavourite({ favourited, propertyId, favouriteId }) {
+  const { id: userId } = useContext(UserContext);
   const [asset, setAsset] = useState("assets/blackHeart.svg");
 
-  useEffect(() => {
-    if (favourited) setAsset("assets/redPinkHeart.svg");
-  }, [favourited]);
+  const handleClick = async () => {
+    if (favourited) {
+      await deleteFavourite(favouriteId);
+      setAsset("assets/blackHeart.svg");
+    } else {
+      await postFavourite(propertyId, userId);
+      setAsset("assets/redPinkHeart.svg");
+    }
+  };
 
-  return <FavouriteButton $asset={asset} />;
+  return <FavouriteButton $asset={asset} onClick={handleClick} />;
 }
