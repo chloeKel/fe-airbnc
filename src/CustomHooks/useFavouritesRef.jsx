23865c15dcbc19c67abb5972329212c5fb4e2702
legshog <chloekelleher@hotmail.co.uk>
2025-03-05 19:33:38 +0000

    updated useFavouritesRef custom hook

---
+++ src/CustomHooks/useFavouritesRef.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect, useContext } from "react";
+import { ErrorContext, UserContext } from "../Contexts/Contexts";
+import { setErrorMsg } from "../Utils/setErrorMsg";
+import { fetchFavourites } from "../Utils/api";
+
+export default function useFavouritesRef() {
+  const { id } = useContext(UserContext);
+  const { setError } = useContext(ErrorContext);
+  const [favouritesRef, setFavouritesRef] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        console.log("useFavouritesRef is being envoked");
+        const { favourites } = await fetchFavourites(id);
+        console.log("favourites:", favourites);
+        const ref =
+          favourites.length > 0
+            ? favourites.reduce((acc, { property_id, favourite_id }) => {
+                acc[property_id] = favourite_id;
+                return acc;
+              }, {})
+            : [];
+
+        console.log("favouritesRef:", favouritesRef);
+        setFavouritesRef(ref);
+      } catch (error) {
+        setError(setErrorMsg(error.response));
+      }
+    })();
+  }, []);
+
+  return { favouritesRef };
+}
